eb437d9b0e17b495e6d325886423d248142bbe5f
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2023-03-07 17:06:55 +0700

    refactor/feature: refactor structure and add feture for calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './bravo-calculator-app/calculator/calculator.component';
-import { NumberPadComponent } from './bravo-calculator-app/components/number-pad/number-pad.component';
-import { HistoryScreenComponent } from './bravo-calculator-app/components/history-screen/history-screen.component';
-import { DisplayScreenComponent } from './bravo-calculator-app/components/display-screen/display-screen.component';
+import { CalculatorModule } from './bravo-calculator-app/calculator.module';
 
 @NgModule({
-	declarations: [AppComponent, CalculatorComponent, NumberPadComponent, HistoryScreenComponent, DisplayScreenComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, CalculatorModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })

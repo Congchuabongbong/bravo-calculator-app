48940c6d1f300dc30c79d90fffbbb7024c9e4e2d
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2023-03-07 14:51:49 +0700

    chore: format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './bravo-calculator-app/calculator/calculator.component';
+import { NumberPadComponent } from './bravo-calculator-app/components/number-pad/number-pad.component';
+import { HistoryScreenComponent } from './bravo-calculator-app/components/history-screen/history-screen.component';
+import { DisplayScreenComponent } from './bravo-calculator-app/components/display-screen/display-screen.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, CalculatorComponent, NumberPadComponent, HistoryScreenComponent, DisplayScreenComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

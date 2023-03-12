f9752e3423134f53c20ab1ca44d0f62badbfd4d8
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2023-03-12 14:31:47 +0700

    refactor: add index export for init-app

--- projects/bravo-calculator/src/lib/bravo-calculator.module.ts
+++ projects/bravo-calculator/src/lib/bravo-calculator.module.ts
@@ -7,12 +7,12 @@ import { NumberPadComponent } from './components/number-pad/number-pad.component
 import { CalculatorInvoker } from './core/command/invoker.service';
 import { ReducerService } from './core/redux/reducers.service';
 import { Store } from './core/redux/store.service';
-import { calculatorReceiver, initialStateCalculator } from './init-app/initial-state';
-import { INITIAL_STATE_CALCULATOR, RECEIVER_TOKEN } from './init-app/token';
+import { calculatorReceiver, initialStateCalculator } from './init-app';
+import { INITIAL_STATE_CALCULATOR, RECEIVER_TOKEN } from './init-app';
 @NgModule({
 	declarations: [BravoCalculatorComponent, DisplayScreenComponent, HistoryScreenComponent, NumberPadComponent],
 	imports: [CommonModule],
 	exports: [BravoCalculatorComponent],
-	providers: [{ provide: INITIAL_STATE_CALCULATOR, useValue: initialStateCalculator }, { provide: RECEIVER_TOKEN, useValue: calculatorReceiver, useClass: CalculatorInvoker }, ReducerService, Store, CalculatorInvoker]
+	providers: [{ provide: INITIAL_STATE_CALCULATOR, useValue: initialStateCalculator }, { provide: RECEIVER_TOKEN, useValue: calculatorReceiver, useClass: CalculatorInvoker }, ReducerService, Store, CalculatorInvoker],
 })
-export class BravoCalculatorModule { }
+export class BravoCalculatorModule {}

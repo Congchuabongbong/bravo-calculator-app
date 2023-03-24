26ceaf26741d5da9abc57c7d8bac4dd591e5bb7a
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2023-03-24 16:28:28 +0700

    refactor: xóa component thừa

--- projects/bravo-calculator/src/lib/bravo-calculator.module.ts
+++ projects/bravo-calculator/src/lib/bravo-calculator.module.ts
@@ -5,15 +5,13 @@ import { CalculatorInvoker } from './core/command/invoker.service';
 import { ReducerService } from './core/redux/reducers.service';
 import { Store } from './core/redux/store.service';
 import { calculatorReceiver, initialStateCalculator, INITIAL_STATE_CALCULATOR, RECEIVER_TOKEN } from './init-app';
-import { DisplayScreenComponent } from './shared/components/display-screen/display-screen.component';
-import { HistoryScreenComponent } from './shared/components/history-screen/history-screen.component';
-import { NumberPadComponent } from './shared/components/number-pad/number-pad.component';
+
 import { FormsModule } from '@angular/forms';
 import { ThousandsSeparatorPipe } from './shared/pipes/thousandsSeparator.format';
 import { WjCoreModule } from '@grapecity/wijmo.angular2.core';
 import { WjInputModule } from '@grapecity/wijmo.angular2.input';
 @NgModule({
-	declarations: [BravoCalculatorComponent, DisplayScreenComponent, HistoryScreenComponent, NumberPadComponent, ThousandsSeparatorPipe],
+	declarations: [BravoCalculatorComponent, ThousandsSeparatorPipe],
 	imports: [CommonModule, FormsModule, WjCoreModule, WjInputModule],
 	exports: [BravoCalculatorComponent],
 	providers: [{ provide: INITIAL_STATE_CALCULATOR, useValue: initialStateCalculator }, { provide: RECEIVER_TOKEN, useValue: calculatorReceiver }, ReducerService, Store, CalculatorInvoker],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { BravoCalculatorModule } from 'projects/bravo-calculator/src/public-api'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule, BravoCalculatorModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

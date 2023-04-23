d706f2d56f40323ca0387c390b6b28b4cc99a972
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2023-04-23 09:28:57 +0700

    refactor: delete lib bravo-editor.

--- projects/bravo-editor/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of bravo-editor
- */
-
-export * from './lib/bravo-editor.service';
-export * from './lib/bravo-editor.component';
-export * from './lib/bravo-editor.module';
